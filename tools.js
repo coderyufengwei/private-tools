975ae67840cd4853dfb900a6ef24db76d5df259a
coderyufengwei <jeff_in_guangdong@163.com>
2021-12-29 18:06:35 +0800

    获取URL？后面的参数

--- tools.js
+++ tools.js
@@ -2,11 +2,27 @@
  * 判断微信浏览器
  * @returns {Boolean}
  */
-function isWeChat() {
+export const isWeixin = () => {
     let str = window.navigator.userAgent.toLowerCase();
     if (str.match(/MicroMessenger/i) == 'micromessenger') {
         return true;
     } else {
         return false;
     }
+}
+
+/* 
+ *获取URL？后面的参数
+ */
+export const getRequest = () => {
+    let url = location.search;
+    let urlRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            urlRequest[strs[i].split("=")[0]] = (strs[i].split("=")[1]);
+        }
+    }
+    return urlRequest;
 }
