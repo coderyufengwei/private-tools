fa51766e739f8427b2ba0ef0c77a21701a19361f
coderyufengwei <jeff_in_guangdong@163.com>
2021-12-30 18:20:50 +0800

    去除字符串空格

--- tools.js
+++ tools.js
@@ -1,5 +1,5 @@
 /**
- * 判断微信浏览器
+ * @desc判断微信浏览器
  * @returns {Boolean}
  */
 export const isWeixin = () => {
@@ -11,8 +11,8 @@ export const isWeixin = () => {
     }
 }
 
-/* 
- *获取URL？后面的参数
+/**
+ *@desc:获取URL？后面的参数
  */
 export const getRequest = () => {
     let url = location.search;
@@ -25,4 +25,23 @@ export const getRequest = () => {
         }
     }
     return urlRequest;
+};
+
+/**
+ *@desc:去除空格 
+ *@params type:  1-所有空格  2-前后空格  3-前空格 4-后空格
+ */
+export const trimy = (str, type = 2) => {
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, '')
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, '')
+        case 3:
+            return str.replace(/(^\s*)/g, '')
+        case 4:
+            return str.replace(/(\s*$)/g, '')
+        default:
+            return str
+    }
 }
