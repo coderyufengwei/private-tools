fa9e8b3d342e06309fc57c4942fb831b5d7c564d
coderyufengwei <jeff_in_guangdong@163.com>
2021-12-30 18:32:55 +0800

    浏览器全屏

--- tools.js
+++ tools.js
@@ -9,10 +9,10 @@ export const isWeixin = () => {
     } else {
         return false;
     }
-}
+};
 
 /**
- *@desc:获取URL？后面的参数
+ * @desc:获取URL？后面的参数
  */
 export const getRequest = () => {
     let url = location.search;
@@ -28,8 +28,8 @@ export const getRequest = () => {
 };
 
 /**
- *@desc:去除空格 
- *@params type:  1-所有空格  2-前后空格  3-前空格 4-后空格
+ * @desc:去除空格 
+ * @params type:  1-所有空格  2-前后空格  3-前空格 4-后空格
  */
 export const trimy = (str, type = 2) => {
     switch (type) {
@@ -44,4 +44,52 @@ export const trimy = (str, type = 2) => {
         default:
             return str
     }
-}
+};
+
+/**
+ * @desc 浏览器全屏切换
+ */
+export const fullscreenToggel = () => {
+    if (fullscreenEnable()) {
+        exitFullScreen();
+    } else {
+        reqFullScreen();
+    }
+};
+
+/**
+ * @desc 判断浏览器是否全屏
+ */
+export const fullscreenEnable = () => {
+    var isFullscreen =
+        document.isFullScreen ||
+        document.mozIsFullScreen ||
+        document.webkitIsFullScreen;
+    return isFullscreen;
+};
+
+/**
+ * @desc 浏览器全屏
+ */
+export const reqFullScreen = () => {
+    if (document.documentElement.requestFullScreen) {
+        document.documentElement.requestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+        document.documentElement.webkitRequestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    }
+};
+
+/**
+ * @desc 浏览器退出全屏
+ */
+export const exitFullScreen = () => {
+    if (document.documentElement.requestFullScreen) {
+        document.exitFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+        document.webkitCancelFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.mozCancelFullScreen();
+    }
+};
