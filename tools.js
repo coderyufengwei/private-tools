583a2a9f8c7f476871711b3426731aea64348d92
coderyufengwei <jeff_in_guangdong@163.com>
2021-12-30 19:05:31 +0800

    字符串大小写转换

--- tools.js
+++ tools.js
@@ -129,3 +129,35 @@ export const loadStyle = url => {
     const head = document.getElementsByTagName("head")[0];
     head.appendChild(link);
 };
+
+/**
+ * @desc 字符串大小写转换 
+ * @params type 1:首字母大写  2：首页母小写  
+ * 3：大小写转换  4：全部大写  5：全部小写 
+ */
+export const changeCase = function (str, type = 4) {
+    switch (type) {
+        case 1:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase()
+            })
+        case 2:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase()
+            })
+        case 3:
+            return str.split('').map(function (word) {
+                if (/[a-z]/.test(word)) {
+                    return word.toUpperCase()
+                } else {
+                    return word.toLowerCase()
+                }
+            }).join('')
+        case 4:
+            return str.toUpperCase()
+        case 5:
+            return str.toLowerCase()
+        default:
+            return str
+    }
+}
